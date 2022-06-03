787e4a61b32c15845d00540433d96f2719700f5c
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-03 16:08:28 -0500

    Removing unnecessary JS files

--- 03-classes/modules/Driver.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Customer_1 = require("./Customer");
-var myCustomer = new Customer_1.Customer("Foo", "Bar");
-console.log(myCustomer.firstName);
-console.log(myCustomer.lastName);
-console.log("".concat(myCustomer.firstName, " ").concat(myCustomer.lastName));
