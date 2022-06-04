8a87f4f865714a9412012c00a3bc4440923c0214
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-04 17:53:08 -0500

    created an array
    created dummy information to display

--- angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SalesPerson } from './sales-person';
 
 @Component({
   selector: 'app-sales-person-list',
@@ -8,6 +9,15 @@ import { Component, OnInit } from '@angular/core';
 export class SalesPersonListComponent implements OnInit {
 
   //create an array
+  salesPersonList: SalesPerson[] = [
+    new SalesPerson("Abc5", "Xyz5", "this_is_an5@email.com", 500),
+    new SalesPerson("Abc6", "Xyz6", "this_is_an6@email.com", 600),
+    new SalesPerson("Abc7", "Xyz7", "this_is_an7@email.com", 700),
+    new SalesPerson("Abc8", "Xyz8", "this_is_an8@email.com", 800),
+    new SalesPerson("Abc9", "Xyz9", "this_is_an9@email.com", 900),
+    new SalesPerson("Abc10", "Xyz10", "this_is_an10@email.com", 1000)
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
