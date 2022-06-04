c66c4a86e74c333624c9d51aa6e555c3162bacfa
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-03 19:30:32 -0500

    changing random components
    learning my way around an angular project also testing the functional integrity of the project

--- angular-training/my-first-angular-project/src/app/app.component.ts
+++ angular-training/my-first-angular-project/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-first-angular-project';
+  title = 'Hello from AppComponent. ';
+
+  firstName = 'Jon';
+  lastName = 'Raz0r';
 }
