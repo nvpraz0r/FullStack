e8f96c307ee0a9c3608af5ba14d8c7892c022321
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-04 17:25:50 -0500

    generated sales-person-list component
    also generated sales-person class to construct the variables

---
+++ angular-training/sales-project/src/app/sales-person-list/sales-person-list.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sales-person-list',
+  templateUrl: './sales-person-list.component.html',
+  styleUrls: ['./sales-person-list.component.css']
+})
+export class SalesPersonListComponent implements OnInit {
+
+  //create an array
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
