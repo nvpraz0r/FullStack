67ac22fec3c76f4825fe793e33152ee8ee770bfd
nvpraz0r <32584149+nvpraz0r@users.noreply.github.com>
2022-06-11 18:08:12 -0500

    generated player-list

--- ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './components/player-list/player-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PlayerListComponent
   ],
   imports: [
     BrowserModule
